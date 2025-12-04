dae96cd81c88c775979b75f88fcd3d83c16cfede
hamzbriel <hamzahgabriel@gmail.com>
2025-12-04 16:46:12 +0700

    model kategori poduk supplier

--- GrosirTubesPBO/src/model/Produk.java
+++ GrosirTubesPBO/src/model/Produk.java
@@ -9,5 +9,82 @@ package model;
  * @author Personal
  */
 public class Produk {
-    
+    private int idProduk;
+    private int idKategori;
+    private int idSupplier;
+    private String namaProduk;
+    private double harga;
+    private int stok;
+
+    // Constructor Kosong
+    public Produk() {
+    }
+
+    // Constructor Lengkap (Untuk mengambil data dari DB)
+    public Produk(int idProduk, int idKategori, int idSupplier, String namaProduk, double harga, int stok) {
+        this.idProduk = idProduk;
+        this.idKategori = idKategori;
+        this.idSupplier = idSupplier;
+        this.namaProduk = namaProduk;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    // Constructor untuk Insert
+    public Produk(int idKategori, int idSupplier, String namaProduk, double harga, int stok) {
+        this.idKategori = idKategori;
+        this.idSupplier = idSupplier;
+        this.namaProduk = namaProduk;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    // Setter dan Getter
+    public int getIdProduk() {
+        return idProduk;
+    }
+
+    public void setIdProduk(int idProduk) {
+        this.idProduk = idProduk;
+    }
+
+    public int getIdKategori() {
+        return idKategori;
+    }
+
+    public void setIdKategori(int idKategori) {
+        this.idKategori = idKategori;
+    }
+
+    public int getIdSupplier() {
+        return idSupplier;
+    }
+
+    public void setIdSupplier(int idSupplier) {
+        this.idSupplier = idSupplier;
+    }
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public void setNamaProduk(String namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
 }
--- GrosirTubesPBO/src/model/Supplier.java
+++ GrosirTubesPBO/src/model/Supplier.java
@@ -9,5 +9,60 @@ package model;
  * @author Personal
  */
 public class Supplier {
-    
+    private int idSupplier;
+    private String namaSupplier;
+    private String telepon;
+    private String alamat;
+
+    // Constructor Kosong
+    public Supplier() {
+    }
+
+    // Constructor Lengkap (Untuk mengambil data dari DB)
+    public Supplier(int idSupplier, String namaSupplier, String telepon, String alamat) {
+        this.idSupplier = idSupplier;
+        this.namaSupplier = namaSupplier;
+        this.telepon = telepon;
+        this.alamat = alamat;
+    }
+
+    // Constructor untuk Insert
+    public Supplier(String namaSupplier, String telepon, String alamat) {
+        this.namaSupplier = namaSupplier;
+        this.telepon = telepon;
+        this.alamat = alamat;
+    }
+
+    // Setter dan Getter
+    public int getIdSupplier() {
+        return idSupplier;
+    }
+
+    public void setIdSupplier(int idSupplier) {
+        this.idSupplier = idSupplier;
+    }
+
+    public String getNamaSupplier() {
+        return namaSupplier;
+    }
+
+    public void setNamaSupplier(String namaSupplier) {
+        this.namaSupplier = namaSupplier;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
 }
