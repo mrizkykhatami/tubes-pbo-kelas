bbb797d2ba111ae13d4b0800d9703e5085612ad8
hamzbriel <hamzahgabriel@gmail.com>
2025-12-04 20:42:09 +0700

    supplier form

--- GrosirTubesPBO/src/view/layout/main/MainMenu.java
+++ GrosirTubesPBO/src/view/layout/main/MainMenu.java
@@ -2,6 +2,7 @@ package view.layout.main;
 
 import view.component.DefaultForm;
 import view.component.KategoriForm;
+import view.component.SupplierForm;
 import view.component.HomeForm;
 import java.awt.Component;
 import view.layout.menu.MenuEvent;
@@ -72,7 +73,7 @@ public class MainMenu extends javax.swing.JFrame {
                     showForm(new DefaultForm("Master 1")); // ganti panel
                 }
                 else if(index == 1 && subIndex == 2){
-                    showForm(new DefaultForm("Master 2")); // ganti panel
+                    showForm(new SupplierForm()); // ganti panel
                 }
                 else if(index == 1 && subIndex == 3){
                     showForm(new KategoriForm()); // ganti panel
